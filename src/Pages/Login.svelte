f54d93db0a8314984ae52b686e34da99d97b1aec
Serumz <65189904+Serumz@users.noreply.github.com>
2021-11-15 14:08:34 +0000

    Created Standard HTML for Login

--- src/Pages/Login.svelte
+++ src/Pages/Login.svelte
@@ -1,8 +1,32 @@
 <script>
-    // your script goes here
+    import Car from "../Components/CarSVG.svelte"
 </script>
+
+<h1>SHUber</h1>
+<Car/>
+<p>Aren't SHU glad you chose us?</p>
+
+<form>Existing User
+    <label for="userName">
+        <input type="text" placeholder="Username / Email Address" name="userName" required>
+    </label>
+    <label for="password">
+        <input type="password" placeholder="Password" name="password" required>
+        <input type="checkbox" name="showPassword">
+    </label>
+    <label for="rememberMe">
+        <input type="checkbox" name="rememberMe">
+    </label>
+
+    <button>Login</button>
+    <button>Forgot Password</button>
+
+    <label for="signUpBtn">New User?
+        <button>Sign Up</button>
+    </label>
+</form>
 
 
 <style>
-
+    
 </style>
