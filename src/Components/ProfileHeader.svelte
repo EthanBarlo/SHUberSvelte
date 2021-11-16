c61028c054b16069607b5b27eebeb3a58cc359bc
Serumz <65189904+Serumz@users.noreply.github.com>
2021-11-16 17:14:06 +0000

    Added ProfileHeader Component
    Co-Authored-By: gipsyjaeger <61258513+gipsyjaeger@users.noreply.github.com>

---
+++ src/Components/ProfileHeader.svelte
@@ -0,0 +1,47 @@
+<script>
+let src = "https://media.discordapp.net/attachments/897035019153977344/910205894397145138/mel_anger.jpg";
+
+export let Name = "Unassigned";
+export let Description = "Unassigned";
+export let Rating = "Unassigned";
+
+</script>
+
+<div id="ProfileContainer">
+    <img {src} alt="Profile Picture">
+    <h2>{Name}</h2>
+    <h3>{Description}</h3>
+    <p>{Rating}⭐</p>
+    <p class="underline"> </p>
+</div>
+
+<style>
+    #ProfileContainer{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    img, h2, h3, p{
+        margin: 0;
+    }
+    img{
+        width: 40%;
+        border-radius: 100%;
+    }
+    p{
+        position: relative;
+    }
+    p::after{
+        position: absolute;
+        content: ' ';
+        width: 150%;
+        height: 10%;
+        background-color: black;
+        bottom: -10%;
+        left: -40%;
+    }
+
+  
+ 
+    
+</style>
--- src/Pages/YourTrips.svelte
+++ src/Pages/YourTrips.svelte
@@ -1,8 +1,10 @@
 <script>
+    import ProfileHeader from "../Components/ProfileHeader.svelte";
     let tempAction = () => {
         console.log("Yay thing might happen?!");
     }
     let trips = [
+        // add map of trip to each item within array (coordinates? (Long/lat))
         {Title:"wow", Cost:"priceMuch", Action: () => {console.log("Wow Special")}},
         {Title:"ewasdas", Cost:"priceMuch", Action:tempAction},
         {Title:"asdasd", Cost:"priceMuch", Action:tempAction},
@@ -12,42 +14,41 @@
 
 </script>
 
+
+<ProfileHeader 
+src="https://media.discordapp.net/attachments/897035019153977344/910205894397145138/mel_anger.jpg" 
+Name="Bill Gates" 
+Description="SHUber User and Apple founder" 
+Rating="4.8"/>
+
+<h1>Your Trips</h1>
 <div id="TripsGrid">
     {#each trips as trip}
         <div class="tripContainer">
             <h2>{trip.Title}</h2>
             <p>{trip.Cost}</p>
-            
+            <!-- Hide img/map of trip within this loop -->
+            <!-- <img src={trip.map} alt="this is a map"> -->
             <button on:click|trusted={trip.Action}>View Trip</button>
         </div>
     {/each}
 </div>
 
-<!-- <div class="grid-container">
-    <div class="grid-item"> 
-        <a href="mars.html">Mars</a>
-        <img src="images/marsLink.jpg" alt="Mars" class="image">
-    </div>
-    <div class="grid-item">
-         <a href="europa.html">Europa</a>
-         <img src="images/europaLink.jpg" alt="Europa" class="image">
-    </div>
-    <div class="grid-item">
-         <a href="lunar.html">Lunar</a>
-          <img src="images/moonLink.jpg" alt="Lunar" class="image">
-    </div>  
-  </div> -->
-
-
 
 <style>
+    h1{
+        width: 100%;
+        text-align: center;
+    }
+
     #TripsGrid{
         display: grid;
         grid-column-gap: 30px;
         grid-row-gap: 30px;
-        
         grid-template-rows: auto auto;
         grid-template-areas: "focused focused";
+        -ms-overflow-style: none;  /* IE and Edge */
+  		scrollbar-width: none;  /* Firefox */
     }
     .tripContainer{
         padding: 0;
@@ -62,20 +63,9 @@
     .tripContainer:focus-within{
         grid-area: focused;
     }
+    /* Show img / map when focused */
+    /* .tripContainer:focus-within img{
+        display: inherit;
+    } */
 
-    /* .grid-container {
-  display: grid;
-  grid-column-gap: 30px;
-  grid-template-columns: auto auto auto;
-  background-color: black;
-  padding: 10px;
-}
-
-.grid-item {
-  background-color: black;
-  border: 1px solid rgba(0, 0, 0, 0.8);
-  padding: 60px;
-  font-size: 18px;
-  text-align: center;
-} */
 </style>
