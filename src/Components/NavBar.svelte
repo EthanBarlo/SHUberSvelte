c255d347ed1fb38bbc4209f93cbd9e3e3ffb8c09
gipsyjaeger <61258513+gipsyjaeger@users.noreply.github.com>
2021-11-29 14:06:40 +0000

    Update NavBar.svelte

--- src/Components/NavBar.svelte
+++ src/Components/NavBar.svelte
@@ -1,18 +1,27 @@
 <script>
+  import { slide } from "svelte/transition"
   import NotificationBell from "./NotificationBell.svelte";
   import BackButton from "./BackButton.svelte";
   export let BackDestination = "#/";
 </script>
 
+
 <div class="navbar">
   <NotificationBell />
   <BackButton Destination={BackDestination}/>
+
+
 </div>
+
+
 
 <style>
   .navbar {
     background-color: #000;
     height: 6%;
     margin: 0;
+    position: relative;
   }
+
+ 
 </style>
