76959ee0883ea00db861f736ab55674455dbc327
Sian Llewellyn <sian.jade.llewellyn@gmail.com>
2021-11-30 15:04:15 +0000

    make Mel small again

--- src/Pages/TripDetails.svelte
+++ src/Pages/TripDetails.svelte
@@ -5,66 +5,48 @@
   import ProfileHeader from "../Components/ProfileHeader.svelte";
   export let tripID = 0;
 
-    let tripLoaded = false;
+  let tripLoaded = false;
 
-    let trip;
-    CurrentUser.subscribe(user => {
-        trip = user.rideHistory.find(ride => ride.id == tripID);
-        tripLoaded = trip != null;
-    });
-    
+  let trip;
+  CurrentUser.subscribe((user) => {
+    trip = user.rideHistory.find((ride) => ride.id == tripID);
+    tripLoaded = trip != null;
+  });
 </script>
 
 <NavBar BackDestination="#/userProfile" />
 <div class="PageContainer">
-    <h1>Trip Details</h1>
-    {#if tripLoaded}
-         <h3>{trip.origin.name} ➜ {trip.destination.name}</h3>
-         <h4>{trip.cost}   :   {trip.travelTime}</h4>
-         <RouteView origin={trip.origin.coords} destination={trip.destination.coords}/>
-     
-         <section>
-             <div id="DriverContainer">
-                 <ProfileHeader
-                 src="https://media.discordapp.net/attachments/897035019153977344/910205894397145138/mel_anger.jpg"
-                 Name="Mel"
-                 Size="Small"
-               />
-             </div>
-             <div id="LatestUpdate">
-                 <h3>Latest Update -</h3>
-                 <p>{trip.status}</p>
-             </div>
-             <div>
-                 <h3>Car Details -</h3>
-                 <p>Reg Plate: </p>
-                 <p>Make: </p>
-                 <p>Colour: </p>
-             </div>
-         </section>
-    {:else}
-         <p>Loading Trip Data...</p>
-    {/if}
+  <h1>Trip Details</h1>
+  {#if tripLoaded}
+    <h3>{trip.origin.name} ➜ {trip.destination.name}</h3>
+    <h4>{trip.cost} : {trip.travelTime}</h4>
+    <RouteView
+      origin={trip.origin.coords}
+      destination={trip.destination.coords}
+    />
 
-  <section>
-    <div id="DriverContainer">
-      <ProfileHeader
-        src="https://media.discordapp.net/attachments/897035019153977344/910205894397145138/mel_anger.jpg"
-        Name="Mel"
-        Size="Small"
-      />
-    </div>
-    <div id="LatestUpdate">
-      <h3>Latest Update -</h3>
-      <p>{trip.status}</p>
-    </div>
-    <div>
-      <h3>Car Details -</h3>
-      <p>Reg Plate:</p>
-      <p>Make:</p>
-      <p>Colour:</p>
-    </div>
-  </section>
+    <section>
+      <div id="DriverContainer">
+        <ProfileHeader
+          src="https://media.discordapp.net/attachments/897035019153977344/910205894397145138/mel_anger.jpg"
+          Name="Mel"
+          Size="Small"
+        />
+      </div>
+      <div id="LatestUpdate">
+        <div id="info">
+          <h3>Latest Update -</h3>
+          <p>{trip.status}</p>
+          <h3>Car Details -</h3>
+          <p>Reg Plate:</p>
+          <p>Make:</p>
+          <p>Colour:</p>
+        </div>
+      </div>
+    </section>
+  {:else}
+    <p>Loading Trip Data...</p>
+  {/if}
 </div>
 
 <style>
@@ -81,8 +63,18 @@
     margin: 0;
   }
 
-  .route {
+  #DriverContainer {
+    width: 100px;
     display: block;
-    height: 50%;
+    margin: 0 auto;
+  }
+
+  #LatestUpdate {
+    display: block;
+    margin: 0 auto;
+  }
+
+  #info {
+    margin: 0 auto;
   }
 </style>
